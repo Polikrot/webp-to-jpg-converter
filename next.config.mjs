7e1165b8f96cd48608824040d2769616cdf28a0d
Polikrot <saltsaler@gmail.com>
2024-10-14 22:51:34 +0200

    Fix path module in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,20 @@
-/**
- * @type {import('next').NextConfig}
- */
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    pageExtensions: ['js', 'jsx', 'ts', 'tsx'],
-    experimental: {
-      esmExternals: true
+  reactStrictMode: true,
+  pageExtensions: ['js', 'jsx', 'ts', 'tsx'],
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': path.resolve(__dirname, './')
     }
-  };
-  
-  export default nextConfig;
+    return config
+  }
+};
+
+export default nextConfig;
